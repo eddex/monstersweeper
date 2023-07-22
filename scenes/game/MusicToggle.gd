db71eb04a71f88214ef13d5132055b8417236b5b
eddex <eddex@outlook.com>
2023-07-22 19:54:42 +0200

    fix: set correct musig toggle button texture scenes are loaded

--- scenes/game/MusicToggle.gd
+++ scenes/game/MusicToggle.gd
@@ -3,6 +3,14 @@ extends Label
 var sound_on = preload("res://assets/kenney-ui-pack/red_boxCheckmark.png")
 var sound_off = preload("res://assets/kenney-ui-pack/red_boxCross.png")
 
+func _ready():
+	_set_button_texture()
+
+
 func _on_SoundSettingButton_pressed():
 	AudioManager.toggle_sound_on_off()
+	_set_button_texture()
+
+
+func _set_button_texture():
 	$SoundSettingButton.texture_normal = sound_on if AudioManager.sound_on else sound_off
